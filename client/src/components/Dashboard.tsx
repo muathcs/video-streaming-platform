b58c86301e1ca4b3132b5ab6203c295f62458bc6
muath <muath.khalifa@yahoo.com>
2023-12-24 07:34:40 +0000

    fixed Dashboard Table, now requests show on there

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -30,20 +30,17 @@ function Dashboard() {
       },
       {
         Header: "Type",
-        accessor: "type",
+        accessor: "req_type",
       },
       {
         Header: "Action",
-        accessor: "action",
+        accessor: "requestaction",
       },
       {
         Header: "message",
         accessor: "message",
       },
-      {
-        Header: "Email",
-        accessor: "email",
-      },
+
       {
         Header: "Completion",
         accessor: "universityx",
@@ -54,13 +51,11 @@ function Dashboard() {
 
   useEffect(() => {
     const fetchRequests = async () => {
-      console.log("fetching");
       try {
         const response = await axios.get("http://localhost:3001/dashboard", {
           params: { uid: currentUser.uid },
         });
 
-        console.log("res: ", response.data);
         setRequests(response.data);
       } catch (error) {
         console.error("error: ", error);
@@ -71,44 +66,55 @@ function Dashboard() {
   }, []);
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+    useTable({
+      columns,
+      data: requests,
+    });
 
   return (
-    <div className=" flex justify-center items-center overflow-hidden ">
-      <div className="container bg-gray-400 w-full h-[70%]  rounded-lg overflow-auto border-4 border-gray-500   ">
-        <table
-          className=" w-full  top-0 relative border-separate border-spacing-0 "
-          {...getTableProps()}
-        >
-          <thead className="w-full sticky top-0 ">
-            {headerGroups.map((headerGroup: any) => (
-              <tr className="" {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column: any) => (
-                  <th className="   bg-gray-400 px-10 py-10 border-2 border-gray-500  ">
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody className="bg-purple-400 w-full overflow-auto ">
-            <td>requests</td>
-            {requests.map((req: any) => {
-              prepareRow(req);
-              return (
-                <tr className="" {...req.getRowProps()}>
-                  {req.cells.map((cell: any) => (
-                    <td className="border-2 border-gray-500 bg-gray-800 px-10  py-10">
-                      {cell.render("Cell")}
-                    </td>
+    <>
+      <div className=" flex justify-center items-center overflow-hidden ">
+        <div className="container bg-gray-400 w-full h-[70%]  rounded-lg overflow-auto border-4 border-gray-500   ">
+          <table
+            {...getTableProps()}
+            className=" w-full  top-0 relative border-separate border-spacing-0 "
+            // {...getTableProps()}
+          >
+            <thead>
+              {headerGroups.map((headerGroup: any) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column: any) => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </th>
                   ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              ))}
+            </thead>
+            <tbody className="bg-purple-400 w-full overflow-auto ">
+              {requests.length != 0 &&
+                rows.map((row: any) => {
+                  console.log("requestS:: ", row);
+                  prepareRow(row);
+
+                  return (
+                    <tr className="" {...row.getRowProps()}>
+                      {row.cells.map((cell: any) => (
+                        <td
+                          {...cell.getCellProps()}
+                          className="border-2 border-gray-500 bg-gray-800 px-10  py-10"
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
