18a0c0377fec597f2a7aee70dbbeebc2f52162a6
muath <muath.khalifa@yahoo.com>
2024-01-19 05:09:52 +0000

    updated custom hook for S3 potenital fix

--- client/src/hooks/useS3Upload.tsx
+++ client/src/hooks/useS3Upload.tsx
@@ -1,5 +1,13 @@
 import { useState, useCallback } from "react";
-import s3 from "../utilities/S3";
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+  secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+  region: "eu-west-2", // Replace with your AWS region
+});
+
+export default s3;
 
 export function useS3Upload() {
   const [s3FileUrl, setS3FileUrl] = useState<string>("");
