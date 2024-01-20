d3aba826dab2be961f0e92f8ba915579fc815d13
muath <muath.khalifa@yahoo.com>
2024-01-20 19:42:38 +0000

    removed cors, using res.header, remoed wrong local host fetch call

--- server/index.js
+++ server/index.js
@@ -36,6 +36,22 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ["https://video-streaming-client.onrender.com"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // multer middleware
