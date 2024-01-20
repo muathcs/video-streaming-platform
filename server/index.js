4b90abf0b76e2c80cb83d5c4b7ccadb5605746a5
muath <muath.khalifa@yahoo.com>
2024-01-20 19:57:32 +0000

    still fixing cors

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://video-streaming-client.onrender.com/",
+      "https://video-streaming-client.onrender.com",
       "http://localhost:5173",
     ],
     credentials: true,
