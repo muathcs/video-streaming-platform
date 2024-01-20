7b0ba059c5b7b415ca71ebc9df9570b65d4b4617
muath <muath.khalifa@yahoo.com>
2024-01-20 19:32:40 +0000

    removed cors, using res.header

--- server/index.js
+++ server/index.js
@@ -21,11 +21,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 // middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://video-streaming-client.onrender.com",
-  })
-);
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://video-streaming-client.onrender.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+
+  console.log("Request received:", req.method, req.url);
+
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
