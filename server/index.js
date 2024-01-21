f4a082ad3a610aadd9aa1c4abe2a856b28b88b8f
muath <muath.khalifa@yahoo.com>
2024-01-21 21:05:24 +0000

    updated s3 profile img upload on signup, now goes through server first

--- server/index.js
+++ server/index.js
@@ -72,21 +72,23 @@ app.use(express.json());
 // });
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
   const allowedOrigins = [
     "https://video-streaming-client.onrender.com",
     "http://localhost:5173",
   ];
   const origin = req.headers.origin;
+
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
+
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Credentials", true); // Corrected typo here
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+
   next();
 });
 
