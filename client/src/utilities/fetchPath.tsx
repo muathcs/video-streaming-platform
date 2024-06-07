2faf584dffafdac8026c8631ec346598979212b7
muath <muath.khalifa@yahoo.com>
2024-06-07 18:38:32 +0100

    Fixing bug on request route

--- client/src/utilities/fetchPath.tsx
+++ client/src/utilities/fetchPath.tsx
@@ -1,6 +1,6 @@
 console.log("urlxx: ", import.meta.env.VITE_API_BASE_URL);
 export const apiUrl: string =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+  "https://video-streaming-server-z2fg.onrender.com" || "http://localhost:3001";
 
 // "https://video-streaming-zrdw.onrender.com"
 // https://video-streaming-server-z2fg.onrender.com/
