7deab6225f35ace7ed75a7183045aacd60ddaf3c
muath <muath.khalifa@yahoo.com>
2024-07-01 22:36:51 +0100

    removed files

--- client/src/utilities/fetchPath.tsx
+++ client/src/utilities/fetchPath.tsx
@@ -1,6 +1,5 @@
 console.log("urlxx: ", import.meta.env.VITE_API_BASE_URL);
-export const apiUrl: string =
-  "https://video-streaming-server-z2fg.onrender.com" || "http://localhost:3001";
+export const apiUrl: string = "http://localhost:3001";
 
 // "https://video-streaming-zrdw.onrender.com"
 // https://video-streaming-server-z2fg.onrender.com/
